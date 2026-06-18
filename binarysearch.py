7bf7ccfb23b62902e28ffdddc33c9c18161f7fb6
midhunpandiyan84-cell <midhunpandiyan84@gmail.com>
2026-06-18 12:55:19 +0530

    Add files via upload
    def binary_search(arr, target):
        left = 0
        right = len(arr) - 1
    
        while left <= right:
            mid = (left + right) // 2
    
            if arr[mid] == target:
                return mid
            elif arr[mid] < target:
                left = mid + 1
            else:
                right = mid - 1
    
        return -1
    
    
    numbers = [1, 2, 4, 7, 9, 12, 15]
    result = binary_search(numbers, 9)
    
    if result != -1:
        print("Found at index:", result)
    else:
        print("Not found")

---
+++ binarysearch.py
@@ -0,0 +1,24 @@
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+
+numbers = [1, 2, 4, 7, 9, 12, 15]
+result = binary_search(numbers, 9)
+
+if result != -1:
+    print("Found at index:", result)
+else:
+    print("Not found")
